90f515b5a4de84b5e6500fdbf623861e31c4b7a3
A1x1On <ura280@mail.ru>
2015-07-21 18:52:59 +0300

    added db context and base reistration of user

--- TaskManager/TaskManager/Controllers/AccountController.cs
+++ TaskManager/TaskManager/Controllers/AccountController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
+using TaskManager.Realizations;
 
 namespace TaskManager.Controllers
 {
@@ -10,11 +14,41 @@ namespace TaskManager.Controllers
     {
         //
         // GET: /Account/
-
+        private string m_ResultMassage = "";
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Registration of User [HttpPost]
+        /// </summary>
+        /// <param name="u">Object of data register</param>
+        /// <returns>Redirect to Index of Manager controller having string m_ResultMassage parametre containing result of UserToDb(u) method</returns>
+        [HttpPost]
+        public ActionResult Registration(USERS u)
+        {
+            RealizeAccount reg = new RealizeAccount();
+            
+             //if (this.Session["CapthaImageText"].ToString() == CaptchaText.ToString())
+            //{
+                if (ModelState.IsValid)
+                {  
+                    m_ResultMassage = reg.UserToDb(u);
+                    ModelState.Clear();
+
+                }
+                
+          //  }
+                return RedirectToAction("Index", "Manager", new { m_ResultMassage });
+  
+        }
+
 
     }
 }
---
+++ TaskManager/TaskManager/Realizations/RealizeAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskManager.Realizations
+{
+
+    
+    public interface IAccount
+    {
+
+        string UserToDb(USERS u);
+
+    }
+
+
+    
+    public class RealizeAccount : IAccount
+    {
+        private string m_Message = "";
+        public string UserToDb(USERS u)
+        {
+            using (TaskManagerEntities db = new TaskManagerEntities())
+            {
+
+                db.USERS.Add(u);
+                db.SaveChanges();
+
+                return "Пользователь успешно зарегистрирован, для подтверждения перейдите на почту";
+
+
+            }
+            return "Ошибка контекста";
+        }
+
+        
+     
+
+
+
+    }
+}
